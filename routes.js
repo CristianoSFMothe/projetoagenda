1a26aca6fc522538a1a0b3733fe9fa75152e88b8
CristianoSFMothe <cristianodevsystem@gmail.com>
2023-09-08 16:10:05 -0300

    :up:  criando a tela de login e signup :bug:  erro ao renderizar

--- routes.js
+++ routes.js
@@ -2,10 +2,16 @@ const express = require('express');
 const route = express.Router();
 
 const homeController = require('./src/controllers/homeController');
+const loginController = require('./src/controllers/loginController');
+const signupController = require('./src/controllers/signupController');
 
 
 // Rotas da home
 route.get('/', homeController.index);
+
+// Rotas da home
+route.get('/login/index', loginController.index);
+route.get('/signup/index', signupController.index);
 
 
 module.exports = route;
