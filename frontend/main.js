da732e94b3dec72738f8854a780f848008d7a769
CristianoSFMothe <cristianodevsystem@gmail.com>
2023-09-08 11:01:06 -0300

    :recycle:  :rocket: refatoração total do código

--- frontend/main.js
+++ frontend/main.js
@@ -1,13 +1,6 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
-import Login from './modules/Login';
+import './assets/css/style.css';
 
-const login = new Login('.form-login');
-const cadastro = new Login('.form-cadastro');
-
-login.init();
-cadastro.init();
-
-// import './assets/css/style.css';
-
+console.log('Olá mundo 3');
